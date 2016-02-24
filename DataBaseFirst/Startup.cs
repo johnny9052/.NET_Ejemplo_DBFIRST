b683e9b130e4f26e6e4955ce65d116c46f4846ec
johnny9052 <alexander9052@gmail.com>
2016-02-24 16:24:18 -0500

    Commit inicial

---
+++ DataBaseFirst/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DataBaseFirst.Startup))]
+namespace DataBaseFirst
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
